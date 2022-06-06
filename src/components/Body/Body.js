bbd337888451a3f751e4aecdadf5d85e27eea2a9
Anjana Poudel <anzana.neutroline@gmail.com>
2022-06-06 15:31:19 +0545

    responsive stuff remained

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./Body.css";
+import bgimage from "../../../src/bgimage.png";
+import microsoft from "../../../src/microsoft.png";
+import nginx from "../../../src/nginx.png";
 
 const Body = () => {
   return (
@@ -10,8 +13,16 @@ const Body = () => {
                   Get your team in sync,no matter your location.<br/>Streamline process,create team rituals,and <br/>watch productivity soar.
               </div>
               <button>Learn more</button>
+              <div className='logos'>
+                  <img src={microsoft} alt="" />
+                  <img src={nginx} alt="" />
+                  <img src={microsoft} alt="" />
+                  <img src={nginx} alt="" />
+              </div>
           </div>
-          <div className='body-image'></div>
+          <div className='body-image'>
+              <img src={bgimage} alt="" />
+          </div>
     </div>
   )
 }
